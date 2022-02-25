ab053303ab808d3063a6d4d7c5d86f40c93b2e61
JansariDivya <98458371+JansariDivya@users.noreply.github.com>
2022-02-25 09:48:15 +0530

    Add files via upload

---
+++ Return_by_Reference_Function.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+int& max(int &,int &);
+int main()
+{
+    
+    int a=5,b=6,ans;
+    ans = max(a,b);
+    cout<<"Maximum="<<ans;
+    
+
+    return 0;
+}
+
+int& max(int &x,int &y)
+{
+    if(x>y)
+	x++
+    return (x);
+    else
+	y++
+    return (y);
+}
+
+
