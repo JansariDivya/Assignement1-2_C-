659b57342a1f590703360200757afffafb7a047b
JansariDivya <98458371+JansariDivya@users.noreply.github.com>
2022-03-21 23:21:45 +0530

    Create Static Variable with Class Template.cpp

---
+++ Static Variable with Class Template.cpp
@@ -0,0 +1,41 @@
+//Program to implement Class template with static variable
+
+#include<iostream>
+using namespace std;
+
+template <class T>
+class Decrement
+{
+	public:
+	void displayincri()
+	{
+		static T i=10.6;
+		cout<<++i<<endl;
+		cout<<"i: "<<i<<endl;
+	}
+};//end of class Decrement
+
+template <class T1>
+class increment
+{
+	public:
+	void displaydecri()
+	{
+		static T1 i=10.6;
+		cout<<--i<<endl;
+		cout<<"i: "<<i<<endl;
+	}
+};//end of class Increment
+
+
+
+int main()
+{
+	increment<int>ob1;  //object of increment class
+	Decrement<double>ob2,ob3; //object of decrement class
+	ob1.displaydecri(); //calling function of increment class
+	ob2.displayincri();  //calling function of decrement class
+	ob3.displayincri();
+return 0;
+}
+
